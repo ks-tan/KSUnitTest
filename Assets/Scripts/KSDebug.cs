e330d73db1fa09fc1765e90cde0a5c434610d3f7
kstan <tankangsoon@gmail.com>
2020-03-01 17:17:20 +0800

    Simple assert method and classes created

---
+++ Assets/Scripts/KSDebug.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KSDebug
+{
+	public static void Log(string logMessage, string color = null)
+	{
+		if (!string.IsNullOrEmpty(color))
+		{
+			logMessage = "<color=" + color + ">" + logMessage + "</color>";
+		}
+
+		Debug.Log(logMessage);
+	}
+}
---
+++ Assets/Scripts/KSUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class KSUnitTest
+{
+	public class Menu
+	{
+		[MenuItem("KS Tools/Run Unit Tests")]
+		static void RunUnitTests()
+		{
+			Assert.AreEquals("asdasdsad", "asdasdsad", true);
+			Assert.AreEquals("asd", "asdasdasd", true);
+			Assert.AreEquals(2, 1 + 1, true);
+			Assert.AreEquals(3, 1 + 1, true);
+		}
+	}
+
+	public class Assert
+	{
+		public static bool AreEquals<T>(T expected, T received, bool shouldShowSuccessLog = false)
+		{
+			bool success = expected.Equals(received);
+
+			if (!success || shouldShowSuccessLog)
+			{
+				string message = success ? "Passed!" : "Failed! Expected: " + expected + ". Received: " + received;
+				KSDebug.Log(message, success ? "green" : "red");
+			}
+
+			return success;
+		}
+	}
+}
+
+public class KSTestClass : Attribute { }
+
+public class KSTestSetup : Attribute { }
+
+public class KSTestMethod : Attribute { }
+
+public class KSTestTeardown : Attribute { }
