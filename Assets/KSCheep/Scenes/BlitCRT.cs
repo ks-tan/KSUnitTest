8b4d1ba8a08a55082652e9c12c6ce9fffedf7362
kstan <tankangsoon@gmail.com>
2020-03-07 23:39:15 +0800

    crt effect

---
+++ Assets/KSCheep/Scenes/BlitCRT.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class BlitCRT : MonoBehaviour
+{
+	[SerializeField] private Material _material = null;
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		Graphics.Blit(source, destination, _material);
+	}
+}
