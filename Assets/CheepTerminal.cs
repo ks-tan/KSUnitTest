70b15a5d1f3de81f082fab5da766f246e1bf9b69
kstan <tankangsoon@gmail.com>
2020-03-07 22:44:08 +0800

    Replaced custom input field implementation with text mesh pro input field. Evaluator works.

---
+++ Assets/CheepTerminal.cs
@@ -0,0 +1,62 @@
+﻿using KSCheep.CodeAnalysis;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+public class CheepTerminal : MonoBehaviour
+{
+	[SerializeField] private TMP_InputField _inputField = null;
+	[SerializeField] private TMP_InputField _consoleField = null;
+
+	private StringBuilder _stringBuilder = new StringBuilder();
+
+	IEnumerator InvokeOnNextFrame(System.Action onComplete)
+	{
+		yield return new WaitForEndOfFrame();
+		onComplete();
+	}
+
+	void OnGUI()
+	{
+		Event e = Event.current;
+
+		if (e.type == EventType.KeyDown)
+		{
+			if (e.keyCode == KeyCode.Return)
+			{
+				string[] inputLines = _inputField.text.Split('\n');
+				_stringBuilder.Clear();
+
+				if (inputLines[inputLines.Length - 1].ToLower() == "clear")
+				{
+					StartCoroutine(InvokeOnNextFrame(() => { _inputField.text = ""; _consoleField.text = ""; }));
+				}
+				else
+				{
+					foreach (var input in inputLines)
+					{
+						var syntaxTree = SyntaxTree.Parse(input);
+
+						if (syntaxTree.Diagnostics.Any())
+						{
+							foreach (var diagnostic in syntaxTree.Diagnostics)
+							{
+								_stringBuilder.AppendLine(diagnostic);
+							}
+						}
+						else
+						{
+							var evaluator = new Evaluator(syntaxTree.Root);
+							var result = evaluator.Evaluate();
+							_stringBuilder.AppendLine(result.ToString());
+						}
+					}
+
+					_consoleField.text = _stringBuilder.ToString();
+				}
+			}
+		}
+	}
+}
