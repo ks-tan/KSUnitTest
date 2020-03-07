9dbd35c14bc40d32b7fdc46ff74ede979718f61f
kstan <tankangsoon@gmail.com>
2020-03-07 19:33:29 +0800

    Cursor for text field

--- Assets/KSCheep/Scenes/KSCheepController.cs
+++ Assets/KSCheep/Scenes/KSCheepController.cs
@@ -1,55 +1,84 @@
-﻿using TMPro;
+﻿using System;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KSCheepController : MonoBehaviour
 {
-	[SerializeField] private TextMeshProUGUI _inputTextUGUI = null;
-	[SerializeField] private TextMeshProUGUI _consoleTextUGUI = null;
+	[SerializeField] private Text _inputTextUI = null; // the input text field
+	[SerializeField] private Text _inputCursorUI = null; // the text field that holds the cursor
 
-	private string _inputText = "";
-	private int _currentInputPosition = 0;
+	private string _inputText = ""; // the entire input field's text
+	private int _currentCursorIndex = 0; // the index on the input string our cursor should be at
 
-	private const char BACKSPACE = '\b';
-	private const char RETURN = '\r';
+	private const char BACKSPACE = '\b'; // backspace character
+	private const char RETURN = '\r'; // return character
 
 	void OnGUI()
 	{
 		Event e = Event.current;
 
+		// if user presses a key
 		if (e.type == EventType.KeyDown && e.keyCode != KeyCode.None)
 		{
-			string left = _inputText.Substring(0, _currentInputPosition);
-			string right = _inputText.Substring(Mathf.Min(_currentInputPosition, _inputText.Length), Mathf.Max(0, _inputText.Length - _currentInputPosition));
+			// split current line to left and right tokens, based on _currentCursorIndex
+			string left = _inputText.Substring(0, _currentCursorIndex);
+			string right = _inputText.Substring(Mathf.Min(_currentCursorIndex, _inputText.Length), Mathf.Max(0, _inputText.Length - _currentCursorIndex));
 
-			if (e.keyCode == KeyCode.RightArrow && _currentInputPosition < _inputText.Length)
+			// right arrow
+			if (e.keyCode == KeyCode.RightArrow && _currentCursorIndex < _inputText.Length)
 			{
-				_currentInputPosition++;
+				_currentCursorIndex++;
 			}
-			else if (e.keyCode == KeyCode.LeftArrow && _currentInputPosition > 0)
+			// left arrow
+			else if (e.keyCode == KeyCode.LeftArrow && _currentCursorIndex > 0)
 			{
-				_currentInputPosition--;
+				_currentCursorIndex--;
 			}
+			else if (e.keyCode == KeyCode.UpArrow)
+			{
+				// find last new line from current position
+				// find offset of current position from last new line
+				// find the last, last new line
+				// if no last, last new line, break..
+				// .. else, apply that calculated offset on last, last newline
+			}
+			else if (e.keyCode == KeyCode.DownArrow)
+			{
+				// find last new line from current position
+				// find offset of current position from last new line
+				// find the next new line
+				// if no next new line, break..
+				// .. else, apply that calculated offset on next newline
+			}
+			// backspace
 			else if (e.keyCode == KeyCode.Backspace)
 			{
-				if (_currentInputPosition > 0)
+				if (_currentCursorIndex > 0)
 				{
 					left = left.Remove(left.Length - 1, 1);
 					_inputText = left + right;
-					_currentInputPosition--;
+					_currentCursorIndex--;
 				}
 			}
+			// return/enter/newline
 			else if (e.keyCode == KeyCode.Return)
 			{
 				_inputText = left + '\n' + right;
-				_currentInputPosition++;
+				_currentCursorIndex++;
 			}
+			// other characters and digits
 			else if (Input.inputString.Length >= 0)
 			{
 				_inputText = left + Input.inputString + right;
-				_currentInputPosition += Input.inputString.Length;
+				_currentCursorIndex += Input.inputString.Length;
 			}
 
-			_inputTextUGUI.SetText(_inputText);
+			_inputTextUI.text = _inputText;
+
+			string cursorText = _inputText.Substring(0, Mathf.Max(0, _currentCursorIndex));
+			_inputCursorUI.text = cursorText + '_';
 		}
 	}
 }
